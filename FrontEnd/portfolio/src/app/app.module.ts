d3a64662a953b4114dedeb4bfb5fa0794da58941
Emmendieta <emmendieta12@gmail.com>
2022-09-25 22:06:11 -0300

    About
    Correcion del Header Static, y creacion del componente About

--- FrontEnd/portfolio/src/app/app.module.ts
+++ FrontEnd/portfolio/src/app/app.module.ts
@@ -8,6 +8,8 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import { SocialsNetsComponent } from './components/socials-nets/socials-nets.component';
 import { HeaderComponent } from './components/header/header.component';
+import { AboutComponent } from './components/about/about.component';
+import {MatCardModule} from '@angular/material/card';
 
 
 
@@ -17,6 +19,7 @@ import { HeaderComponent } from './components/header/header.component';
     BannerComponent,
     SocialsNetsComponent,
     HeaderComponent,
+    AboutComponent,
   ],
   imports: [
     BrowserModule,
@@ -24,6 +27,7 @@ import { HeaderComponent } from './components/header/header.component';
     BrowserAnimationsModule,
     MatToolbarModule,
     MatIconModule,
+    MatCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ FrontEnd/portfolio/src/app/components/about/about.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.css']
+})
+export class AboutComponent implements OnInit {
+  longText = `The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog
+  from Japan. A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was
+  originally bred for hunting.`;
+  Nombre = "Mendieta Emiliano Manuel";
+  Titulo = "Programador Full-Stack Jr."
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
